88442055f6d20dc907aeba4d15b8386a6a6c6377
Javier Lindo <javi.lindo@gmail.com>
2024-02-16 19:11:09 +0100

    jenkinsfile de PRO con nuevo Python

--- pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
+++ pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -x
-python3.7 -m venv todo-list-aws
+python3.11 -m venv todo-list-aws
 source todo-list-aws/bin/activate
-python -m pip install --upgrade pip
-python -m pip install awscli
-python -m pip install aws-sam-cli
-# For integration testing
-python -m pip install pytest
+python3 -m pip install --upgrade pip
+#For integration testing
+python3 -m pip install pytest
+# For HTTP requests
+python3 -m pip install requests
+
 pwd
