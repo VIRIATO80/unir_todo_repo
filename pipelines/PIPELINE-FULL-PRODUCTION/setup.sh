dae2c765d9d61cd631dc43bdc44442e3c70b12ae
Javier Lindo <javi.lindo@gmail.com>
2024-02-16 19:09:16 +0100

    cambios setup

--- pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
+++ pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 set -x
-python3.10 -m venv todo-list-aws
-python -m pip install pytest
-python -m pip install requests
+python3.11 -m venv todo-list-aws
+source todo-list-aws/bin/activate
+python3 -m pip install --upgrade pip
+#For integration testing
+python3 -m pip install pytest
+# For HTTP requests
+python3 -m pip install requests
 
 pwd
